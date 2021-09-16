c69d24779d1b6da48d90f01812e82ef13204afc0
HasinduLanka <hlrathnayaka@gmail.com>
2021-09-16 17:02:00 +0530

    creds

---
+++ endpoints/creds.go
@@ -0,0 +1,9 @@
+package endpoints
+
+var (
+	Mongo_proto  string = "mongodb+srv://"
+	Mongo_user   string = "username"
+	Mongo_pass   string = "apsppasss"
+	Mongo_host   string = "minicluster.hybfy.mongodb.net"
+	Mongo_DBName string = "database"
+)
