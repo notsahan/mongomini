7c88bd0591144c5c219b7ff9c50125739a95dd4f
HasinduLanka <hlrathnayaka@gmail.com>
2021-09-16 16:20:20 +0530

    creds ignore

--- api/creds.go
+++ api/creds.go
@@ -3,7 +3,7 @@ package handler
 var (
 	Mongo_proto  string = "mongodb+srv://"
 	Mongo_user   string = "minivercel"
-	Mongo_pass   string = "password"
+	Mongo_pass   string = "apsppasss"
 	Mongo_host   string = "minicluster.hybfy.mongodb.net"
 	Mongo_DBName string = "database"
 )
