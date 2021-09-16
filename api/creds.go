b9c4ff0bf21dcc7f9ea0aff14af450f42deb3490
notsahan <84143965+notsahan@users.noreply.github.com>
2021-09-16 15:04:54 +0530

    Update creds.go

--- api/creds.go
+++ api/creds.go
@@ -1,8 +1,9 @@
 package handler
 
 var (
+	Mongo_proto  string = "mongodb+srv://"
 	Mongo_user   string = "username"
 	Mongo_pass   string = "password"
+	Mongo_host   string = "minicluster.hybfy.mongodb.net"
 	Mongo_DBName string = "database"
-	
 )
